6c2adfd9a8c1f32800810b63e8e6bae532f1e879
Petalo <luis.lorenzo2@alu.uclm.es>
2025-10-02 23:04:16 +0200

    Device by Petalo

---
+++ src/AppForSEII2526.API/Models/Device.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public double PriceForPurchase { get; set; }
+        public double PriceForRent { get; set; }
+        public int QuantityForPurchase { get; set; }
+        public int QuantityForRent { get; set; }
+        public int Year { get; set; }
+
+        // Relación muchos-a-uno con Model
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        // Relación uno-a-muchos con RentDevice
+        public IList<RentDevice> RentedDevices { get; set; } = new List<RentDevice>();
+
+        // Otras relaciones (puedes definir las clases si las necesitas)
+        public IList<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
+        public IList<ReviewItem> ReviewItems { get; set; } = new List<ReviewItem>();
+
+        public override bool Equals(object obj) => obj is Device d && this.Id == d.Id;
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
