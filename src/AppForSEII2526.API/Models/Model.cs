653b30708652b96353be74a216d58cf594d2a350
Petalo <luis.lorenzo2@alu.uclm.es>
2025-10-02 22:10:42 +0200

    clase model creada by Petalo

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class Model
+{
+    //Atributos 
+    private int id;
+    private string nameModel;
+    private IList<Device> devices;
+
+   / Getters y Setters
+    public int GetId(){
+        return this.id;
+    }
+
+    public void SetId(int id){
+        this.id = id;
+    }
+
+    public string GetNameModel(){
+        return this.nameModel;
+    }
+
+    public void SetNameModel(string nameModel){
+        this.nameModel = nameModel;
+    }
+
+    // Constructores 
+    public Model()
+    {
+        this.devices = new List<Device>();
+    }
+
+    public Model(int id, string nameModel){
+        // Llamada explícita al constructor sin parámetros
+        this(); // Así como lo harías en Java
+        this.id = id;
+        this.nameModel = nameModel;
+    }
+    public int GetId(){
+    return this.id;
+    }
+
+
+public override bool Equals(object obj){
+    return obj is Model m && this.id == m.id;
+}
+
+public override int GetHashCode()
+{
+    return this.id.GetHashCode();
+}
+}
