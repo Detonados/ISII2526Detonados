82df525036d60e32568bc36817a499b66d41ea24
Petalo <luis.lorenzo2@alu.uclm.es>
2025-10-02 22:57:47 +0200

    rent device creado by Petalo

---
+++ src/AppForSEII2526.API/Models/RentDevice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AppForSEII2526.API.Models
+{
+    public class RentDevice
+    {
+        // Propiedades principales
+        public int DeviceId { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int RentId { get; set; }
+
+        // Relaciones de navegación
+        public Device Device { get; set; }
+        public Rental Rent { get; set; }
+
+        // Constructor vacío
+        public RentDevice() { }
+
+        // Constructor con parámetros
+        public RentDevice(int deviceId, int rentId, int quantity, double price)
+        {
+            DeviceId = deviceId;
+            RentId = rentId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        // Sobrescribir Equals
+        public override bool Equals(object obj)
+        {
+            return obj is RentDevice rd &&
+                   rd.DeviceId == this.DeviceId &&
+                   rd.RentId == this.RentId;
+        }
+
+        // Sobrescribir GetHashCode
+        public override int GetHashCode()
+        {
+            return (DeviceId, RentId).GetHashCode();
+        }
+    }
+}
