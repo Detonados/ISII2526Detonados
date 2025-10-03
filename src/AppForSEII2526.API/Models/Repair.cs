cd93e0468c1035db90314f2477d7c594f9d2f0fa
Patrik <patrik.lopes@alu.uclm.es>
2025-10-03 10:14:41 +0200

    Añadida clase Repair

---
+++ src/AppForSEII2526.API/Models/Repair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Repair
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Cost { get; set; }
+    public int ScaleId { get; set; }
+
+    public Scale Scale { get; set; }
+    public List<ReceiptItem> ReceiptItems { get; set; } = new List<ReceiptItem>();
+
+    public Repair() { }
+    public Repair(int id, string name, string description, float cost, int scaleId)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Cost = cost;
+        ScaleId = scaleId;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Repair repair &&
+               Id == repair.Id &&
+               Name == repair.Name &&
+               Description == repair.Description &&
+               Cost == repair.Cost &&
+               ScaleId == repair.ScaleId;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name, Description, Cost, ScaleId);
+    }
+}
+
+
