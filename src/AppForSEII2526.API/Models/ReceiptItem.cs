ab718043c0e52bdbd75aeda9e4cecdaf34568d93
Patrik <patrik.lopes@alu.uclm.es>
2025-10-03 13:22:20 +0200

    Añadida clase ReceiptItem

---
+++ src/AppForSEII2526.API/Models/ReceiptItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class ReceiptItem
+{
+    public string Model { get; set; }
+    public int ReceiptId { get; set; }
+    public int RepairId { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is ReceiptItem other)
+            return ReceiptId == other.ReceiptId && RepairId == other.RepairId && Model == other.Model;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ReceiptId, RepairId, Model);
+    }
+
+    public ReceiptItem() { }
+
+    public ReceiptItem(string model, int receiptId, int repairId)
+    {
+        Model = model;
+        ReceiptId = receiptId;
+        RepairId = repairId;
+    }
+}
