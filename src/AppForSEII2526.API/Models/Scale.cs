71d350e03e6ddf10ae2d5b364d852f1a563d0fe7
Patrik <patrik.lopes@alu.uclm.es>
2025-10-03 10:02:16 +0200

    Añadida clase Scale

---
+++ src/AppForSEII2526.API/Models/Scale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Scale
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Repair> Repairs { get; set; } = new List<Repair>();
+
+    public Scale() { }
+    public Scale(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Scale scale &&
+               Id == scale.Id &&
+               Name == scale.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
+}
+
+
