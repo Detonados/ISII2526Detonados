a83a1c5c7960fd5f3f5e8e0a64044dc8d0f70a2d
Alexuss28 <alejandro.jara1@alu.uclm.es>
2025-10-03 10:16:44 +0200

    Creada la clase Purchase

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        //hola
+        public string CustomerUserName { get; set; }
+        public string CustomerUserSurname { get; set; }
+        public string DeliveryAddress { get; set; }
+        public int Id { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public double TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
+        public IList<PurchaseItem> PurchaseItems { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Purchase other) return false;
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public Purchase()
+        {
+            PurchaseItems = new List<PurchaseItem>();
+        }
+    }
+}
