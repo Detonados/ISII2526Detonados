487384613a3d2f248b080fed8f35e48752eaac7c
Petalo <129395041+Luis-Lorenzo@users.noreply.github.com>
2025-10-07 18:30:03 +0200

    12 rentdevice (#46)
    * rent device creado by Petalo
    
    * cambio
    
    * requisitos y keys

---
+++ src/AppForSEII2526.API/Models/RentDevice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppForSEII2526.API.Models
+{
+    public class RentDevice
+    {
+        // Clave primaria compuesta (DeviceId + RentId)
+        [Key, Column(Order = 0)]
+        [Required(ErrorMessage = "El identificador del dispositivo es obligatorio.")]
+        public int DeviceId { get; set; }
+
+        [Key, Column(Order = 1)]
+        [Required(ErrorMessage = "El identificador del alquiler es obligatorio.")]
+        public int RentId { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser positivo.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
+        public int Quantity { get; set; }
+
+        // Relaciones de navegación
+        [ForeignKey("DeviceId")]
+        public Device Device { get; set; }
+
+        [ForeignKey("RentId")]
+        public Rental Rent { get; set; }
+
+        // Constructor vacío
+        public RentDevice() { }
+
+        // Constructor con parámetros
+        public RentDevice(int deviceId, int rentId, int quantity, double price)
+        {
+            DeviceId = deviceId;
+            RentId = rentId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        // Sobrescribir Equals
+        public override bool Equals(object obj)
+        {
+
+            return obj is RentDevice rd &&
+                   rd.DeviceId == this.DeviceId &&
+                   rd.RentId == this.RentId;
+        }
+
+        // Sobrescribir GetHashCode
+        public override int GetHashCode()
+        {
+            return (DeviceId, RentId).GetHashCode();
+        }
+    }
+}
