1a1dadee43dcce437a9c0c58e3fc204a349a1a76
Dante <patrik.lopes@alu.uclm.es>
2025-10-07 18:44:05 +0200

    15 crear clase scale (#49)
    * Añadida clase Scale
    
    * Añadida libreria de listas en Scale
    
    * Añadidas Keys, Require y mensajes de error

---
+++ src/AppForSEII2526.API/Models/Scale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Scale
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre de la balanza es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
+    public string Name { get; set; }
+
+    public List<Repair> Repairs { get; set; } = new List<Repair>();
+
+    public Scale() { }
+    public Scale(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Scale scale &&
+               Id == scale.Id &&
+               Name == scale.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
+}
+
+
