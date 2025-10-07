1d27298d78de1eb456a4b7c7c50986f1c0bbafff
Alexuss28 <alejandro.jara1@alu.uclm.es>
2025-10-07 10:18:15 +0200

    Creada la clase Purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AppForSEII2526.API.Models
 {
     public class Purchase
     {
-        //hola
-        public string CustomerUserName { get; set; }
-        public string CustomerUserSurname { get; set; }
-        public string DeliveryAddress { get; set; }
+        [Key] // Clave primaria
         public int Id { get; set; }
+
+        [Required] // Requisito
+        [MaxLength(100)]
+        public string CustomerUserName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CustomerUserSurname { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
         public PaymentMethod PaymentMethod { get; set; }
+
+        [Required]
         public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
         public double TotalPrice { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
         public int TotalQuantity { get; set; }
+
+        // Relación uno n con PurchaseItem
         public IList<PurchaseItem> PurchaseItems { get; set; }
 
         public override bool Equals(object obj)
