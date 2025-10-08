923f14e0321bf66601bb8b0c43dee0b3d98b3e0d
Petalo <luis.lorenzo2@alu.uclm.es>
2025-10-08 09:10:35 +0200

    añadido ReviewItem clase de Device para la  reseña

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReviewItem {
+        //Clase de funcionamiento al igual qye PaymentMethod
+        [Required(ErrorMessage = "El valor es obligatorio.")]
+        [Range(0, 5, ErrorMessage = "El valor debe estar entre 0 y 5.")]
+        public int? Rating { get; set; }
+        [Required(ErrorMessage = " El comentario no puede estar vacio.")]
+        [StringLength(500, ErrorMessage = "El comentario no puede superar los 500 caracteres.")]
+        public string? Comment { get; set; }
+
+    }
+}
