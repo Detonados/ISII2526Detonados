f3afcb89bd5e5a0e3eac821accf07bea4f6e8019
Petalo <luis.lorenzo2@alu.uclm.es>
2025-10-08 12:43:44 +0200

    aplication user cambiada y relacuones creadas diagrama caso 2 alterado

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -2,6 +2,38 @@
 
 namespace AppForSEII2526.API.Models;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
+public class ApplicationUser : IdentityUser
+{
+    public ApplicationUser()
+    {
+    }
+    public ApplicationUser(string id, string name, string surname, string userName)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        UserName = userName;
+        Email = userName;
+    }
+
+    [Display(Name = "Name")]
+    public string? Name
+    {
+        get;
+        set;
+    }
+
+    //---------------------
+    public IList<Purchase> Purchase { get; set; }
+    public IList<Rental> Rentals { get; set; }
+
+    //---------------------
+
+    [Display(Name = "Surname")]
+    public string? Surname
+    {
+        get;
+        set;
+    }
 }
+
