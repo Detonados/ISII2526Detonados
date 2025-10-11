686562479f1e63ecf85f3afd7d2959a5989364d3
Alexuss28 <alejandro.jara1@alu.uclm.es>
2025-10-11 17:42:53 +0200

    Solución Clave Compuesta en PurchaseItem

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -4,11 +4,9 @@ using DataType = System.ComponentModel.DataAnnotations.DataType;
 
 namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(PurchaseId), nameof(DeviceId))]
     public class PurchaseItem
     {
-        [Key] // Clave primaria
-        public int Id { get; set; }
-
         [Required(ErrorMessage = "La descripción es obligatoria.")]
         [MaxLength(200, ErrorMessage = "La descripción no puede superar los 200 caracteres.")]
         public string? Description { get; set; }
@@ -16,6 +14,7 @@ namespace AppForSEII2526.API.Models
         [Required(ErrorMessage = "El identificador del dispositivo es obligatorio.")]
         [ForeignKey("Device")]
         public int DeviceId { get; set; }
+        public Device Device { get; set; }
 
         [Required(ErrorMessage = "El precio es obligatorio.")]
         [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser igual o mayor que 0.")]
@@ -26,24 +25,23 @@ namespace AppForSEII2526.API.Models
         [Required(ErrorMessage = "El identificador de la compra es obligatorio.")]
         [ForeignKey("Purchase")]
         public int PurchaseId { get; set; }
+        public Purchase Purchase { get; set; }
 
         [Required(ErrorMessage = "La cantidad es obligatoria.")]
         [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
         public int Quantity { get; set; }
 
-        //Relaciones
-        public Device Device { get; set; }
-        public Purchase Purchase { get; set; }
 
         public override bool Equals(object obj)
         {
-            if (obj is not PurchaseItem other) return false;
-            return Id == other.Id;
+            if (obj is PurchaseItem other)
+                return PurchaseId == other.PurchaseId && DeviceId == other.DeviceId && Description == other.Description;
+            return false;
         }
 
         public override int GetHashCode()
         {
-            return Id.GetHashCode();
+            return HashCode.Combine(PurchaseId, DeviceId, Description);
         }
 
         public PurchaseItem()
